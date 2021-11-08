a039e7e1438842a3b0ed862a8c4a842d9279c7e6
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2021-11-08 14:15:25 +0000

    restructured example data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,7 +1,19 @@
 ## code to prepare `DATASET` dataset goes here
-
+prj <- '+proj=utm +zone=30 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0'
 creel <-
   read.table("data-raw/doi_10.5061_dryad.k80bp46__v1.txt",
              header=TRUE, sep=",") %>%
-  select(ID, date, x, y, behaviour)
+  as_tibble() %>%
+  select(id = ID, time = date, x, y, behaviour) %>%
+  mutate(id = ifelse(id == "GP004", "GP005", id),
+         id = as.integer(stringr::str_sub(id, 5)),
+         time = lubridate::ymd_hms(time)) %>%
+  sf::st_as_sf(coords = c("x", "y"),
+               crs = prj,
+               remove = FALSE) %>%
+  sf::st_transform(crs = 4326) %>%
+  mutate(lon = sf::st_coordinates(geometry)[, 1],
+         lat = sf::st_coordinates(geometry)[, 2]) %>%
+  sf::st_drop_geometry() %>%
+  select(id, time, x, y, lon, lat, behaviour)
 usethis::use_data(creel, overwrite = TRUE)
