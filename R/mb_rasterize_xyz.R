189faee0433948cd1198415a09b0b68f70938202
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2023-12-06 08:39:50 +0000

    export mb-function again

--- R/mb_rasterize_xyz.R
+++ R/mb_rasterize_xyz.R
@@ -1,3 +1,13 @@
+#' Rasterize xyz data
+#'
+#' @param xyz A tiblle
+#' @param r0 A base raster
+#' @param agg Level of aggregation (2, 4, ...)
+#' @param fun Normally mean or count
+#'
+#' @return A {terra} raster
+#' @export
+#'
 mb_rasterize_xyz <- function(xyz, r0, agg = 1, fun = "mean") {
 
   my_extent <- function(x) {
