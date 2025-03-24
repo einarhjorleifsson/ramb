3718cfb4b65439d7b1fad08969e4958073c88dbb
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2025-03-24 15:33:17 +0000

    simple rename

--- R/trips.R
+++ R/trips.R
@@ -1,16 +1,18 @@
 #' Create trip
 #'
+#' The function 
+#'
 #' @param x A vector
 #'
 #' @return An integer vector of the same length as input, providing unique trip number
 #' @export
 #'
 rb_trip <- function(x) {
-  tibble::tibble(x = x) %>% 
+  tibble::tibble(x = x) |> 
     dplyr::mutate(tid = dplyr::if_else(x != dplyr::lag(x), 1L, 0L, 1L),
-                  tid = ifelse(x, -tid, tid)) %>% 
-    dplyr::group_by(x) %>% 
-    dplyr::mutate(tid = cumsum(tid)) %>% 
-    dplyr::ungroup(x) %>% 
+                  tid = ifelse(x, -tid, tid))  |>  
+    dplyr::group_by(x) |>  
+    dplyr::mutate(tid = cumsum(tid))  |>  
+    dplyr::ungroup(x) |> 
     dplyr::pull(tid)
 }
