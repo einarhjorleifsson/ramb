9b05d9d18671bcf2d9c0f902577d4b1f7a86f861
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2025-07-31 08:21:27 +0000

    bug fix on cap function

--- R/cap.R
+++ R/cap.R
@@ -4,27 +4,27 @@
 #' values, but if arguement p is of length 2, lower values can also be substituted.
 #'
 #' @param x A numeric vector
-#' @param p Normally a single value (default 0.98), values higher than this will be replaced by this value.
+#' @param limit Normally a single value (default 0.98), values higher than this will be replaced by this value.
 #' Default is the 98th percentile of the distribution of x.
 #' 
 #'
 #' @return A vector of same length as x
 #' @export
 #'
-rb_cap_winsorize <- function(x, p = quantile(x, probs = c(0.98), na.rm = FALSE)) {
+rb_cap_winsorize <- function(x, limit = quantile(x, probs = c(0.98), na.rm = FALSE)) {
   
-  if(length(p) > 2) {
+  if(length(limit) > 2) {
     message("Cap vector can only be of length 2 (upper and lower values)")
   }
-  if(length(p) == 2) {
-    if(p[1L] >= p[2L]) message("Lower bound must be less than upper bound")
+  if(length(limit) == 2) {
+    if(limit[1L] >= limit[2L]) message("Lower bound must be less than upper bound")
   }
   
-  if(length(val) == 1) {
-    x[x > val] <- val
+  if(length(limit) == 1) {
+    x[x > limit] <- limit
   } else {
-    x[x < val[1L]] <- val[1L]
-    x[x > val[2L]] <- val[2L]
+    x[x < limit[1L]] <- limit[1L]
+    x[x > limit[2L]] <- limit[2L]
   }
   
   return(x)
@@ -36,29 +36,29 @@ rb_cap_winsorize <- function(x, p = quantile(x, probs = c(0.98), na.rm = FALSE))
 #' Replaces extreme values by less extreme ones.
 #'
 #' @param x A numeric vector
-#' @param val A single value (default 1.5), log10 step values between consequitve
+#' @param step_limit A single value (default 1.5), log10 step values between consecutive
 #' ordered values of x will be capped by the maximum step value.
 #'
 #' @return A vector of same length as x
 #' @export
 #'
-rb_cap_miller <- function(x, val = 1.5) {
+rb_cap_miller <- function(x, step_limit = 1.5) {
   w <- x |> unique() |> sort()
   w <- w[w > 0]
   if (length(w) > 0) {
     log_w <- log10(w)
     difw <- diff(log_w)
-    if (any(difw > val)) {
-      valid_indices <- which(difw <= val)
+    if (any(difw > step_limit)) {
+      valids <- which(difw <= step_limit)
       
-      if (length(valid_indices) > 0) {
-        res <- w[max(valid_indices) + 1]
+      if (length(valids) > 0) {
+        res <- w[max(valids) + 1]
       } else {
         res <- max(w, na.rm = TRUE)
       }
     }
   }
-  x <- ifelse(x > res, res, x)
+  x[x > res]  <- res
   return(x)
 }
 
